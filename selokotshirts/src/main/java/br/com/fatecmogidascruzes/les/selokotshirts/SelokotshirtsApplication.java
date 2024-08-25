ad88cf1cbfdef90a1ee19722d85357817f9f2107
Lucas José de Almeida Moura <lucasjosemoura649@gmail.com>
2024-08-25 16:20:55 -0300

    add initial backend
    Adicionando Springboot 3.3.3 com lombok, spring Web, mySqlDriver e jpa

---
+++ selokotshirts/src/main/java/br/com/fatecmogidascruzes/les/selokotshirts/SelokotshirtsApplication.java
@@ -0,0 +1,13 @@
+package br.com.fatecmogidascruzes.les.selokotshirts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelokotshirtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelokotshirtsApplication.class, args);
+	}
+
+}
---
+++ selokotshirts/src/test/java/br/com/fatecmogidascruzes/les/selokotshirts/SelokotshirtsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fatecmogidascruzes.les.selokotshirts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelokotshirtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
