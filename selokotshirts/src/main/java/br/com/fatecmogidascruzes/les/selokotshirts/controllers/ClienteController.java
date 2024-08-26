cfa25c21636be594f2c54fdf6d29d6ea5954059d
Lucas José de Almeida Moura <lucasjosemoura649@gmail.com>
2024-08-26 02:01:16 -0300

    Add Initial Services Cliente
    adicionando a service de cliente

--- selokotshirts/src/main/java/br/com/fatecmogidascruzes/les/selokotshirts/controllers/ClienteController.java
+++ selokotshirts/src/main/java/br/com/fatecmogidascruzes/les/selokotshirts/controllers/ClienteController.java
@@ -3,6 +3,7 @@ package br.com.fatecmogidascruzes.les.selokotshirts.controllers;
 import br.com.fatecmogidascruzes.les.selokotshirts.dtos.ClienteRecordDto;
 import br.com.fatecmogidascruzes.les.selokotshirts.models.ClienteModel;
 import br.com.fatecmogidascruzes.les.selokotshirts.repositories.ClienteRepository;
+import br.com.fatecmogidascruzes.les.selokotshirts.services.ClienteService;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,21 +19,22 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
+@CrossOrigin(maxAge = 3600, origins = "localhost:5500")
 public class ClienteController {
 
     @Autowired
-    ClienteRepository clienteRepository;
+    ClienteService clienteService;
 
     @PostMapping("/clientes")
     public ResponseEntity<ClienteModel> salvarCliente(@RequestBody @Valid ClienteRecordDto clienteRecordDto) {
         var clienteModel = new ClienteModel();
         BeanUtils.copyProperties(clienteRecordDto, clienteModel);
-        return ResponseEntity.status(HttpStatus.CREATED).body(clienteRepository.save(clienteModel));
+        return ResponseEntity.status(HttpStatus.CREATED).body(clienteService.save(clienteModel));
     }
 
     @GetMapping("/clientes")
     public ResponseEntity<List<ClienteModel>> listarClientes() {
-        List<ClienteModel> clientesList = clienteRepository.findAll();
+        List<ClienteModel> clientesList = clienteService.findAll();
         if (!clientesList.isEmpty()) {
             for (ClienteModel cliente : clientesList) {
                 UUID id = cliente.getIdCliente();
@@ -44,7 +46,7 @@ public class ClienteController {
 
     @GetMapping("/clientes/{id}")
     public ResponseEntity<Object> listarUmCliente(@PathVariable(value = "id") UUID id) {
-        Optional<ClienteModel> clienteO = clienteRepository.findById(id);
+        Optional<ClienteModel> clienteO = clienteService.findById(id);
         if(clienteO.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cliente Não Encontrado.");
         }
@@ -54,21 +56,21 @@ public class ClienteController {
     @PutMapping("/clientes/{id}")
     public ResponseEntity<Object> atualizarCliente(@PathVariable(value = "id") UUID id,
                                                    @RequestBody @Valid ClienteRecordDto clienteRecordDto) {
-        Optional<ClienteModel> clienteO = clienteRepository.findById(id);
+        Optional<ClienteModel> clienteO = clienteService.findById(id);
         if(clienteO.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cliente Não Encontrado.");
         }
         var clienteModel = clienteO.get();
         BeanUtils.copyProperties(clienteRecordDto, clienteModel);
-        return ResponseEntity.status(HttpStatus.OK).body(clienteRepository.save(clienteModel));
+        return ResponseEntity.status(HttpStatus.OK).body(clienteService.save(clienteModel));
     }
     @DeleteMapping("/clientes/{id}")
     public ResponseEntity<Object> deletarCliente(@PathVariable(value = "id") UUID id) {
-        Optional<ClienteModel> clienteO = clienteRepository.findById(id);
+        Optional<ClienteModel> clienteO = clienteService.findById(id);
         if(clienteO.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cliente Não Encontrado.");
         }
-        clienteRepository.delete(clienteO.get());
+        clienteService.delete(clienteO.get());
         return ResponseEntity.status(HttpStatus.OK).body("Cliente deletado com sucesso.");
     }
 
---
+++ selokotshirts/src/main/java/br/com/fatecmogidascruzes/les/selokotshirts/services/ClienteService.java
@@ -0,0 +1,38 @@
+package br.com.fatecmogidascruzes.les.selokotshirts.services;
+
+import br.com.fatecmogidascruzes.les.selokotshirts.models.ClienteModel;
+import br.com.fatecmogidascruzes.les.selokotshirts.repositories.ClienteRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    ClienteRepository clienteRepository;
+
+    @Transactional
+    public ClienteModel save(ClienteModel clienteModel){
+        return clienteRepository.save(clienteModel);
+    }
+
+    public List<ClienteModel> findAll() {
+        return clienteRepository.findAll();
+    }
+
+    public Optional<ClienteModel> findById(UUID id) {
+        return clienteRepository.findById(id);
+    }
+
+    @Transactional
+    public void delete(ClienteModel clienteModel) {
+        clienteRepository.delete(clienteModel);
+    }
+
+
+}
